b3fffb5e593e19699eaa3e0390f941dc56fa313e
Cary Goeller <carygoeller@gmail.com>
2017-06-05 15:41:46 +0200

    initial commit

---
+++ R/african_name_generator.R
@@ -0,0 +1,27 @@
+
+african_name = function(name = "A", boy = TRUE){
+  #error checkers here
+
+  #gender = toupper(gender)
+
+  if(boy){
+    name = toupper(name)
+    first = substr(name, 0,1)
+    ascii = utf8ToInt(first)
+    name_list = snames[snames$ASCII_1 == ascii, 4]
+    name_list = na.omit(name_list)
+    african = name_list[runif(1,1,nrow(name_list)),1]
+
+  }
+  else{
+    name = toupper(name)
+    first = substr(name, 0, 1)
+    ascii = utf8ToInt(first)
+    name_list = snames[snames$ASCII == ascii, 1]
+    name_list = na.omit(name_list)
+    african = name_list[runif(1,1,nrow(name_list)),1]
+  }
+
+  sprintf("Your South African name is %s", african)
+
+}
